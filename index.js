a54bc5725f3aa022d8ea49d60c55b49cb6b5665d
abhijitbhattacharya <abhijit81kol@gmail.com>
2020-06-13 22:11:53 +0530

    REDUX DEMO 1st Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const BUY_CAKE = "BUY_CAKE";
+
+//REDUC ACTION CTREATOR should me a function, which return an ACTION as Object
+//ACTION is an object with TYPE (here, type: BUY_CAKE) property
+buyCake = () => {
+  return {
+    type: BUY_CAKE,
+    info: "First Rexud Action",
+  };
+};
