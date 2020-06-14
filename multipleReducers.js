51129cd1fe282595408f9c3e275033dd6f3819a8
abhijitbhattacharya <abhijit81kol@gmail.com>
2020-06-14 22:12:29 +0530

    Using redux-logger middleware in multiple reducers

--- multipleReducers.js
+++ multipleReducers.js
@@ -5,6 +5,8 @@ const reduxLogger = require("redux-logger");
 
 const createStore = redux.createStore; // need to import createStore to create a STORE
 const combineReducers = redux.combineReducers; // need to import combineReducers to combine multiple REDUCERS
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 
 //State of my appliation
 const initialCakeState = {
@@ -22,13 +24,13 @@ const BUY_ICECREAM = "BUY_ICECREAM"; // creating a TYPE string for ACTION
 buyCake = () => {
   return {
     type: BUY_CAKE,
-    info: "First Rexud Action",
+    info: "Buy Cake Action",
   };
 };
 buyIceCream = () => {
   return {
     type: BUY_ICECREAM,
-    info: "Second Rexud Action",
+    info: "Buy Icecream Action",
   };
 };
 
@@ -63,16 +65,14 @@ const rootReducer = combineReducers({
 
 // creating a REDUX STORE with createStore from REDUX library.
 // CreateStore method accepts a parameter. The parameter will be REDUCER, which controlls how the transition will happen.
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 // Once STORE is created, I logged that to console and that is initial state of the STORE
 // Output: Initial State { noOfCakes: 10 }
 console.log("Initial State", store.getState());
 
 // After that we setup a listener to the STORE. SO anytime STORE updates, we log that to console
-const unsubscribe = store.subscribe(() =>
-  console.log("Updated state", store.getState())
-);
+const unsubscribe = store.subscribe(() => {});
 
 // when I dispatched the first ACTION (*** I need to pass ACTION method as a parameter in DISPATCH method),
 // the REDUCER sees the ACTION "type" is BUY_CAKE (becasue we made TYPE as BUY_CASE in ACTION object)
